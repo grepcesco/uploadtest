1460549342536459a13217228fcc09102d6bd7d4
Francesco Lorenzon <franzlorenzon@gmail.com>
2013-03-22 11:31:42 +0100

    first commit

---
+++ lang/en/uploadtest.php
@@ -0,0 +1,42 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for uploadtest
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod
+ * @subpackage uploadtest
+ * @copyright  2011 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'uploadtest';
+$string['modulenameplural'] = 'uploadtests';
+$string['modulename_help'] = 'Use the uploadtest module for understand how to upload a file with moodle';
+$string['uploadtestfieldset'] = 'Custom example fieldset';
+$string['uploadtestname'] = 'uploadtest name';
+$string['uploadtest_file'] = 'Upload the file';
+$string['uploadtest_help'] = 'This is the content of the help tooltip associated with the uploadtestname field. Markdown syntax is supported.';
+$string['uploadtest'] = 'uploadtest';
+$string['pluginadministration'] = 'uploadtest administration';
+$string['pluginname'] = 'uploadtest';
